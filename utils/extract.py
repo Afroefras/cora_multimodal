7c390cf2d6e783c4059e9df3187b4098a3ad7569
EF <efraisma.ef7@gmail.com>
2023-03-18 19:59:38 -0600

    Class ExtracData created
    methods read_record and a folder of records created

--- utils/extract.py
+++ utils/extract.py
@@ -1,6 +1,40 @@
-import wfdb
+from pathlib import Path
+from wfdb import processing, rdrecord, plot_wfdb
 
-FILE_DIR = "data/physionet.org/files/ephnogram/1.0.0/WFDB/ECGPCG0004"
+from torch import Tensor
 
-record = wfdb.rdrecord(FILE_DIR) 
-wfdb.plot_wfdb(record=record) 
+
+class ExtractData:
+    def __init__(self) -> None:
+        pass
+
+    def read_record(self, file_dir: str, just_signal: bool) -> Tensor:
+        record = rdrecord(file_dir)
+        if just_signal:
+            record = Tensor(record.p_signal)
+
+        return record
+
+    def read_records_folder(self, folder_dir: str, just_signal: bool) -> list:
+        folder_dir = Path(folder_dir)
+        files_list = folder_dir.glob("*[!.html]")
+        unique_filenames = set(map(lambda x: x.stem, files_list))
+
+        records = []
+        for filename in unique_filenames:
+            file_dir = folder_dir.joinpath(filename)
+            record = self.read_record(file_dir, just_signal)
+            records.append(record)
+
+        return records
+
+
+ed = ExtractData()
+
+FOLDER_DIR = "data/physionet.org/files/ephnogram/1.0.0/WFDB"
+ed.read_records_folder(FOLDER_DIR, just_signal=True)
+
+
+# qrs_inds = processing.qrs.gqrs_detect(sig=record.p_signal[:, 0], fs=record.fs)
+# print('\nGQRS detect: ', qrs_inds)
+# plot_wfdb(record=record)
